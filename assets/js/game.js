ae4628120c2fd9572ea32c9188340126b492d763
Massagli <adrielmassagli@gmail.com>
2023-09-14 19:49:19 -0300

    Add Disappearance

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,12 @@
+
 let altura_tela = 0
 let largura_tela = 0
+
+
+
+
+
+
 
 function ajustaPalco(){
     altura_tela = window.innerHeight
@@ -10,6 +17,13 @@ function ajustaPalco(){
 ajustaPalco()
 
 function posicao_randomica(){
+
+    if(document.querySelector(".imagem_mosquito")){
+       document.querySelector(".imagem_mosquito").remove()
+
+        document.querySelector("#v1").src = "./assets/img/coracao_vazio.png"
+    }
+
     let eixoX = Math.floor(Math.random()*largura_tela)-90;
     let eixoY = Math.floor(Math.random()*altura_tela)-90;
 
@@ -25,5 +39,41 @@ function posicao_randomica(){
     mosquito.className = "imagem_mosquito"
     mosquito.style.left = `${eixoX}px`
     mosquito.style.top = `${eixoY}px`
+    mosquito.style.transform = ladoMosquito()
+    mosquito.id = tamanhoMosquito()
+
+    mosquito.onclick=function(){
+    
+        this.remove()
+        
+    }
+
 }
 
+
+
+function tamanhoMosquito(){
+    let tamanho_mosquito = Math.round(Math.random() * 3)
+
+    switch(tamanho_mosquito){
+        case 1:
+            return "tamanho_1"
+        case 2:
+            return "tamanho_2"
+        case 3:
+            return "tamanho_3"
+        default:
+            return 
+    }
+}
+
+function ladoMosquito(){
+    let lado_mosquito = Math.round(Math.random())
+
+    switch(lado_mosquito){
+        case 0:
+            return 'scaleX(-1)';
+        default:
+            return 'scaleX(1)';
+    }
+}
